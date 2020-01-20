bc601e1d5a715c54962d0a885f263b1a5d682822
Dylan Craig <dylan@Dylans-MBP.fios-router.home>
2020-01-20 17:11:11 -0500

    Add error handling

--- emailUser.js
+++ emailUser.js
@@ -15,56 +15,60 @@ module.exports = async (email, token) => {
     }
   });
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Story Master" <noreply@story-master.com>',
-    to: email,
-    subject: 'Story Master Sign In ✔',
-    html: `
-      <head>
-      <style type="text/css">
-        .container {
-          text-align: center;
-          font-family: Verdana, Geneva, Tahoma, sans-serif;
-        }
-        h1 {
-          color: #3dd969;
-        }
-        a {
-          text-align: center;
-        }
-        button {
-          font-weight: 300;
-          font-family: Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI',
-            Roboto, 'Helvetica Neue', Arial, sans-serif;
-          border: 1px solid transparent;
-          padding: 0.75rem 1.25rem;
-          font-size: 0.875rem;
-          line-height: 1.125;
-          border-radius: 0.375rem;
-          transition: all 250ms cubic-bezier(0.27, 0.01, 0.38, 1.06);
-          color: #fff;
-          border-color: #007bff;
-          background-color: #007bff;
-          box-shadow: none;
-          margin-top: 50px;
-          width: 50%;
-        }
-      </style>
-    </head>
-    <body>
-      <div class="container">
-        <h1>Welcome to Story Master</h1>
-        <p>We sent you this email just to verify that it's really you. Cause ya know, security is important.
-        Click below to sign in!</p>
-        <a href=${
-          process.env.NODE_ENV === 'production' ? productionLink : link
-        }><button>Click here to Sign In</button></a>
-      </div>
-    </body>
-      `
-  });
+  try {
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Story Master" <noreply@story-master.com>',
+      to: email,
+      subject: 'Story Master Sign In ✔',
+      html: `
+        <head>
+        <style type="text/css">
+          .container {
+            text-align: center;
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+          }
+          h1 {
+            color: #3dd969;
+          }
+          a {
+            text-align: center;
+          }
+          button {
+            font-weight: 300;
+            font-family: Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI',
+              Roboto, 'Helvetica Neue', Arial, sans-serif;
+            border: 1px solid transparent;
+            padding: 0.75rem 1.25rem;
+            font-size: 0.875rem;
+            line-height: 1.125;
+            border-radius: 0.375rem;
+            transition: all 250ms cubic-bezier(0.27, 0.01, 0.38, 1.06);
+            color: #fff;
+            border-color: #007bff;
+            background-color: #007bff;
+            box-shadow: none;
+            margin-top: 50px;
+            width: 50%;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <h1>Welcome to Story Master</h1>
+          <p>We sent you this email just to verify that it's really you. Cause ya know, security is important.
+          Click below to sign in!</p>
+          <a href=${
+            process.env.NODE_ENV === 'production' ? productionLink : link
+          }><button>Click here to Sign In</button></a>
+        </div>
+      </body>
+        `
+    });
 
-  console.log('Message sent: %s', info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  } catch (err) {
+    console.error(err.message);
+  }
 };
