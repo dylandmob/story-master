4447d165803ed69c9020c4141938e18691dd3f63
Dylan Craig <dylan@dylans-mbp.lan>
2020-02-09 17:42:13 -0500

    Remove email auth

--- routes/users.js
+++ routes/users.js
@@ -1,63 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const config = require('config');
-const { check, validationResult } = require('express-validator');
-
-const emailUser = require('../emailUser');
 const User = require('../models/User');
 
-// @route   POST api/users
-// @desc    Register a user
-// @access  Public
-router.post(
-  '/',
-  [
-    check('name', 'Please add your name')
-      .not()
-      .isEmpty(),
-    check('email', 'Please include a valid email').isEmail()
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    const { name, email } = req.body;
-
-    try {
-      let user = await User.findOne({ email });
-
-      if (user) {
-        if (user.isVerified) {
-          return res.status(400).json({ msg: 'User already exists' });
-        }
-        await user.remove();
-      }
-
-      user = new User({ name, email }); // Create a new user
-
-      await user.save();
-
-      const payload = {
-        user: {
-          id: user.id
-        }
-      };
-
-      const token = jwt.sign(payload, config.get('jwtEmailSecret'), {
-        expiresIn: config.get('emailTokenLife')
-      });
-
-      emailUser(email, token);
-
-      res.json({ msg: `Email sent to ${email}` });
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
-    }
-  }
-);
+/* TODO 
+  - add getting users
+  - add getting user by id
+  - add deleting user
+*/
 
 module.exports = router;
--- server.js
+++ server.js
@@ -21,6 +21,14 @@ app.use(express.json({ extended: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    else next();
+  });
+}
+
 // Define Routes
 app.use('/auth/google', require('./routes/auth-google'));
 app.use('/api/users', require('./routes/users'));
