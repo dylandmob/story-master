cda3d8acb5af8757b465112f30f6d36f2def0a7b
Dylan <dylan.m.craig21@gmail.com>
2023-01-26 17:35:44 -0500

    change port back

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const passport = require('passport');
 require('./config/passport-setup');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
